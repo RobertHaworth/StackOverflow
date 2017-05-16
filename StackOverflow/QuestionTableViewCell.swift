2f1c4254ce7663d70d9fd1be3cab66959bc5b81c
Robert Haworth <robertchaworth@gmail.com>
2017-05-16 10:42:29 -0500

    Implemented basic question tableViewCell.

---
+++ StackOverflow/QuestionTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  StackOverflowQuestionsTableViewCell.swift
+//  StackOverflow
+//
+//  Created by Robert Haworth on 5/16/17.
+//  Copyright © 2017 Robert Haworth. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var answersCountLabel: UILabel!
+    @IBOutlet weak var viewsCountLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
